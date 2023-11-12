f7f5fd4abd13bf3c6fd348b64757460ec0bea21e
AlexSpy69 <139042727+AlexSpy69@users.noreply.github.com>
2023-11-12 18:30:28 +0100

    Added component class

--- src/geometry_dash_recreation/constants.py
+++ src/geometry_dash_recreation/constants.py
@@ -5,12 +5,14 @@ MONITOR_NR = 1
 width = screeninfo.get_monitors()[MONITOR_NR].width
 height = screeninfo.get_monitors()[MONITOR_NR].height
 
-FPS = 60
-DELTA_TIME = 60 / FPS
-SCREEN_WIDTH = width
-SCREEN_HEIGHT = height
-UNIT = SCREEN_HEIGHT * 0.08
-GROUND_HEIGHT = UNIT * 3
-PLAYER_POS = (UNIT * 5, SCREEN_HEIGHT-GROUND_HEIGHT)
-VEL_ADD = 2
-JUMP_VEL = 25
+FPS = 60                        # Bilder pro Sekunde
+DELTA_TIME = 60 / FPS           # Abstand zwischen zwei Frames in Sekunden multipliziert mit 60
+SCREEN_WIDTH = width            # Fensterbreite
+SCREEN_HEIGHT = height          # Fensterhöhe
+UNIT = SCREEN_HEIGHT * 0.08     # Einheit
+GROUND_HEIGHT = UNIT * 3        # Höhe des Bodens
+PLAYER_POS = (UNIT * 5,
+              SCREEN_HEIGHT-
+              GROUND_HEIGHT)    # Ursprüngliche Position des Spielers auf dem Bildschirm
+VEL_ADD = 2                     # Stärke der Einwirkung der Gravitation
+JUMP_VEL = 25                   # Stärke des Sprungs des Spielers
--- src/geometry_dash_recreation/sprites.py
+++ src/geometry_dash_recreation/sprites.py
@@ -8,7 +8,7 @@ pygame.init()
 class Cube(pygame.sprite.Sprite):
     def __init__(self, *groups: AbstractGroup) -> None:
         super().__init__(*groups)
-        self.image = pygame.image.load('assets/textures/icons/cubes/icon_1.png').convert_alpha()
+        self.image = pygame.image.load("assets/textures/icons/cubes/icon_1.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (UNIT, UNIT))
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.bottom = PLAYER_POS
@@ -41,22 +41,45 @@ class Cube(pygame.sprite.Sprite):
 
 # Background-Sprites
 class Ground(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, *groups: AbstractGroup) -> None:
+        super().__init__(*groups)
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('assets/textures/bg/ground.jpg').convert()
-        image_size = SCREEN_WIDTH, SCREEN_HEIGHT
-        self.image = pygame.transform.scale(self.image, image_size)
+        self.image = pygame.image.load("assets/textures/bg/ground.jpg").convert()
+        self.image = pygame.transform.scale(self.image, (SCREEN_WIDTH, SCREEN_HEIGHT))
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = 0, SCREEN_HEIGHT-GROUND_HEIGHT
 
 class Background(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('assets/textures/bg/background.png').convert()
-        image_size = SCREEN_HEIGHT*4, SCREEN_HEIGHT*2
-        self.image = pygame.transform.scale(self.image, image_size)
+    def __init__(self, *groups: AbstractGroup) -> None:
+        super().__init__(*groups)
+        self.image = pygame.image.load("assets/textures/bg/background.png").convert()
+        self.image = pygame.transform.scale(self.image, (SCREEN_WIDTH*4, SCREEN_HEIGHT*2))
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = 0, -SCREEN_HEIGHT/4
 
     def update(self):
-        self.rect.x -= DELTA_TIME
+        self.rect.x -= DELTA_TIME    # Bewegen des Hintergrunds nach links
+
+# Component-Sprite (für die Hindernisse im Spiel)
+class Component(pygame.sprite.Sprite):
+    def __init__(self, imgfile="assets/textures/transparent.png",
+                 xsize=1, ysize=1, hb_mul=1, *groups: AbstractGroup) -> None:
+        super().__init__(*groups)
+        self.image = pygame.image.load(imgfile).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (UNIT*xsize, UNIT*ysize))
+        self.rect = self.image.get_rect()
+        self.hitbox = self.rect
+        self.hitbox.width = UNIT*xsize*hb_mul    # Der Hitbox kann um einen bestimmten Faktor vergrößert
+        self.hitbox.height = UNIT*ysize*hb_mul   # oder verkleinert werden
+
+        self.type = ""      # platform: Man kann darauf landen und davon abspringen
+                            # hazard: Man stirbt, wenn man es berührt
+                            # deco: Es zählt als Dekoration und wird vom Spieler ignioriert
+                            # ring: Rings, von denen man abspringen kann, wenn man die Maustaste drückt
+                            # gravportal: Portale, die die Gravitationsrichtung wechseln
+                            # formportal: Portale, die den Spielmodus wechseln
+
+        self.color = ""     # Wird als Indentifikation für Rings und Portale verwendet.
+    
+    def update(self):
+        pass
