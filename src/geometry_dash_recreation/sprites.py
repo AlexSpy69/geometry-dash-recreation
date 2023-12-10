ddcfbdfc0f006f88f017216f0f48305bfe87b588
AlexSpy69 <139042727+AlexSpy69@users.noreply.github.com>
2023-12-10 11:56:33 +0100

    Added cube air spin, improved cube landing and added more cube textures

--- src/geometry_dash_recreation/sprites.py
+++ src/geometry_dash_recreation/sprites.py
@@ -9,56 +9,64 @@ pygame.init()
 class Cube(pygame.sprite.Sprite):
     def __init__(self, *groups: AbstractGroup) -> None:
         super().__init__(*groups)
-        self.image = pygame.image.load("assets/textures/icons/cubes/icon_1.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (UNIT, UNIT))
+        self.original_image = pygame.image.load("assets/textures/icons/cubes/icon_2.png").convert_alpha()
+        self.original_image = pygame.transform.scale(self.original_image, (UNIT, UNIT))
+        self.image = self.original_image
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.bottom = PLAYER_POS
-        self.hitbox = self.rect
+        self.hitbox = self.image.get_rect()
+        self.hitbox.left, self.hitbox.bottom = PLAYER_POS
 
         self.vel = 0
+        self.angle = 0
     
     def jump(self, gravity) -> None:
         self.vel = -JUMP_VEL * gravity
     
     def death_touch(self, sprite: pygame.sprite.Sprite) -> bool:
         return self.hitbox.bottom - DEATH_ACCURACY >= sprite.hitbox.top
-    
+
     def controls(self, ev, click, gravity, ground: pygame.sprite.Sprite, level_gr: pygame.sprite.Group) -> int:
         # Springen
         if ev:
             if self.hitbox.bottom == ground.rect.top:
                 self.jump(gravity)
             for sprite in level_gr:
-                if self.hitbox.colliderect(sprite.rect.move(0, -1)):
+                if self.hitbox.colliderect(sprite.hitbox.move(0, -1)):
                     self.jump(gravity)
         
         # Einwirkung der Gravitation
         self.hitbox.y += self.vel * DELTA_TIME * RESIZE
         self.vel += VEL_ADD * gravity * DELTA_TIME
 
+        self.angle -= 7
+
         # Landen auf dem Boden
-        if self.hitbox.colliderect(ground.rect) and gravity == 1:
-            while self.hitbox.colliderect(ground.rect):
-                self.hitbox.y -= DELTA_TIME * gravity
+        if self.hitbox.colliderect(ground.rect.move(0, -1)) and gravity == 1:
+            self.angle = round(self.angle / 90) * 90
+            self.hitbox.bottom = ground.rect.top
             self.vel = 0
         
         # Landen auf Level-Komponenten
         for sprite in level_gr:
             if sprite.type == "platform":
-                if self.hitbox.colliderect(sprite.hitbox) and gravity == 1:
+                if self.hitbox.colliderect(sprite.hitbox.move(0, -1)) and gravity == 1:
                     if self.death_touch(sprite):
                         return 1
-                    while self.hitbox.colliderect(sprite.hitbox):
-                        self.hitbox.y -= DELTA_TIME * gravity
+                    self.angle = round(self.angle / 90) * 90
+                    self.hitbox.bottom = sprite.hitbox.top
                     self.vel = 0
             elif sprite.type == "hazard":
-                if self.hitbox.colliderect(sprite.hitbox):
+                if self.hitbox.colliderect(sprite.hitbox.move(0, -1)):
                     return 1
         
+        self.angle = 0 if self.angle == 360 else self.angle
+
         return 0
     
     def update(self, *args: Any, **kwargs: Any) -> None:
-        self.rect.x, self.rect.y = self.hitbox.x, self.hitbox.y
+        self.image = pygame.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect(center=self.hitbox.center)
         return super().update(*args, **kwargs) 
 
 # Background-Sprites
@@ -96,7 +104,8 @@ class Component(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (UNIT*size[0], UNIT*size[1]))
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = pos[0]*UNIT, pos[1]*UNIT  # Position des Komponenten
-        self.hitbox = self.rect
+        self.hitbox = self.image.get_rect()
+        self.hitbox.x, self.hitbox.y = pos[0]*UNIT, pos[1]*UNIT
         self.hitbox.width, self.hitbox.height = \
             self.rect.width*hb_mul, self.rect.height*hb_mul
         # Der Hitbox kann um einen bestimmten Faktor vergrößert oder verkleinert werden
@@ -112,5 +121,5 @@ class Component(pygame.sprite.Sprite):
     
     def update(self, *args: Any, **kwargs: Any) -> None:
         self.rect.x -= LEVEL_SCROLL_SPEED
-        self.hitbox.x = self.rect.x
+        self.hitbox.center = self.rect.center
         return super().update(*args, **kwargs)
