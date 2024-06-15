ecd38a61db341dd26545162cb9016a0922296197
AlexSpy69 <139042727+AlexSpy69@users.noreply.github.com>
2024-06-15 12:55:30 +0200

    Added newlines to messages at help module

--- src/geometry_dash_recreation/help.py
+++ src/geometry_dash_recreation/help.py
@@ -9,7 +9,7 @@ def print_help():
     """
 
     print("\nUSAGE: geometry_dash_recreation <window-width> <window-height> (for windowed mode)")
-    print("       geometry_dash_recreation <monitor-nr> (for fullscreen mode)")
+    print("       geometry_dash_recreation <monitor-nr> (for fullscreen mode)\n")
 
 
 def print_import():
@@ -20,4 +20,4 @@ def print_import():
     """
 
     print('\nNOTE: geometry_dash_recreation is not meant to be imported and used as a module. '
-          'You have to run it with "python3 -m geometry_dash_recreation".')
+          'You have to run it with "python3 -m geometry_dash_recreation".\n')
