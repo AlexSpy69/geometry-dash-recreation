3239bf3ef045fb81e77058eb82258dc518097f88
AlexSpy69 <139042727+AlexSpy69@users.noreply.github.com>
2024-06-15 12:51:54 +0200

    Added the docstrings to help module

--- src/geometry_dash_recreation/help.py
+++ src/geometry_dash_recreation/help.py
@@ -1,8 +1,23 @@
+"""Modul für das Ausgeben von Hilfetexten."""
+
+
 def print_help():
-    print("\nUSAGE: geometry_dash_recreation <window-width> <window-height>")
-    print("       geometry_dash_recreation <monitor-nr>")
+    """
+    Gibt die Anleitung zur Verwendung des Programms mit den benötigten Parametern in der Kommandozeile aus.
+
+    :return:
+    """
+
+    print("\nUSAGE: geometry_dash_recreation <window-width> <window-height> (for windowed mode)")
+    print("       geometry_dash_recreation <monitor-nr> (for fullscreen mode)")
 
 
 def print_import():
+    """
+    Gibt die Nachricht aus, dass geometry_dash_recreation nicht zum Importieren gedacht ist.
+
+    :return:
+    """
+
     print('\nNOTE: geometry_dash_recreation is not meant to be imported and used as a module. '
           'You have to run it with "python3 -m geometry_dash_recreation".')
